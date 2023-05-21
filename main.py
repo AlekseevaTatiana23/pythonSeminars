119cd6aa7bf26241d5bc77eaa2af362cccc08ec3
Tatiana Alekseeva <alekseeva.tany@list.ru>
2023-05-21 20:04:04 +0300

    семинар1

---
+++ main.py
@@ -0,0 +1,28 @@
+"""
+a=5
+b=5.89
+c='hello'
+
+print(f"{a} - {b} - {c}")
+print("{} - {} - {}".format(a,b,c))"""
+
+
+"""print('Введите первое число: ')
+a=input()
+print(a)
+b=input('Введите первое число: ')"""
+
+"""c=5.89
+print(c)
+print(type(c))
+c=int(c)
+print(c)
+print(type(c))"""
+
+"""a=6.8989
+b=6.8975
+print(round((a*b),3))"""
+
+
+a=1<4
+print(a)
